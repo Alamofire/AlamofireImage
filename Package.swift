ad3caf725d1d6ea45ad13c8534d201b8330a331f
Jon Shier <jon@jonshier.com>
2017-04-17 18:54:48 -0400

    Fix SPM integration.

--- Package.swift
+++ Package.swift
@@ -26,5 +26,6 @@ let package = Package(
     name: "AlamofireImage",
     dependencies: [
         .Package(url: "https://github.com/Alamofire/Alamofire.git", versions: Version(4, 0, 0)..<Version(5, 0, 0))
-    ]
+    ],
+    exclude: ["Tests"]
 )
