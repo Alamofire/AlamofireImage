12f222226200153a245b89400862800936f0bd91
Jon Shier <jon@jonshier.com>
2019-10-27 18:53:12 -0400

    Update version, CHANGELOG.

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Alamofire/Alamofire.git",
-            from: "5.0.0-rc.2"
+            from: "5.0.0-rc.3"
         )
     ],
     targets: [
